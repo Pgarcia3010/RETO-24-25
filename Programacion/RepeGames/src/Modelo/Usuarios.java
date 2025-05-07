753531bc8481f6887045c35054340dbe8fb58ad5
Paul <p_garciaveg@fpzornotza.com>
2025-05-07 12:24:08 +0200

    Javadoc a Usuarios.java

--- Programacion/RepeGames/src/Modelo/Usuarios.java
+++ Programacion/RepeGames/src/Modelo/Usuarios.java
@@ -1,6 +1,7 @@
 package Modelo;
+
 /**
- * 
+ * Sea crea un objeto Usuario.
  * 
  * @author Paul Garcia
  * @version 1.0
@@ -11,82 +12,108 @@ public class Usuarios {
 	private String contrasenya;
 	private int idCliente;
 	private int idEmpleado;
-/**
- * 
- * @param nickname
- * @param contrasenya
- * @param idCliente
- * @param idEmpleado
- */
+
+	/**
+	 * El constructor registra los parametros de la base de datos en la tabla de
+	 * Usuarios.
+	 * 
+	 * @param nickname    El nickname es un String que indica el nickname del
+	 *                    cliente.
+	 * @param contrasenya La contraseña es un String que indica la contraseña del
+	 *                    cliente.
+	 * @param idCliente   La idCliente es un int que indica la id del cliente.
+	 * @param idEmpleado  La idEmpleado es un int que indica la id del empleado.
+	 */
 	public Usuarios(String nickname, String contrasenya, int idCliente, int idEmpleado) {
 		this.nickname = nickname;
 		this.contrasenya = contrasenya;
 		this.idCliente = idCliente;
 		this.idEmpleado = idEmpleado;
 	}
-/**
- * 
- */
+
+	/**
+	 * El constructor esta vacio
+	 */
 	public Usuarios() {
 	}
-/**
- * 
- * @return
- */
+
+	/**
+	 * recibe nickname
+	 * 
+	 * @return nickname El nickname es un String que indica el nickname del cliente.
+	 */
 	public String getNickname() {
 		return nickname;
 	}
-/**
- * 
- * @param nickname
- */
+
+	/**
+	 * modifica nickname
+	 * 
+	 * @param nickname El nickname es un String que indica el nickname del cliente.
+	 */
 	public void setNickname(String nickname) {
 		this.nickname = nickname;
 	}
-/**
- * 
- * @return
- */
+
+	/**
+	 * recibe contrasenya
+	 * 
+	 * @return contrasenya La contraseña es un String que indica la contraseña del
+	 *         cliente.
+	 */
 	public String getContrasenya() {
 		return contrasenya;
 	}
-/**
- * 
- * @param contrasenya
- */
+
+	/**
+	 * modifica contrasenya
+	 * 
+	 * @param contrasenya La contraseña es un String que indica la contraseña del
+	 *                    cliente.
+	 */
 	public void setContrasenya(String contrasenya) {
 		this.contrasenya = contrasenya;
 	}
-/**
- * 
- * @return
- */
+
+	/**
+	 * recibe idCliente
+	 * 
+	 * @return idCliente La idCliente es un int que indica la id del cliente.
+	 */
 	public int getIdCliente() {
 		return idCliente;
 	}
-/**
- * 
- * @param idCliente
- */
+
+	/**
+	 * modifica idCliente
+	 * 
+	 * @param idCliente La idCliente es un int que indica la id del cliente.
+	 */
 	public void setIdCliente(int idCliente) {
 		this.idCliente = idCliente;
 	}
-/**
- * 
- * @return
- */
+
+	/**
+	 * recibe idEmpleado
+	 * 
+	 * @return idEmpleado La idEmpleado es un int que indica la id del empleado.
+	 */
 	public int getIdEmpleado() {
 		return idEmpleado;
 	}
-/**
- * 
- * @param idEmpleado
- */
+
+	/**
+	 * modifica idEmpleado
+	 * 
+	 * @param idEmpleado La idEmpleado es un int que indica la id del empleado.
+	 */
 	public void setIdEmpleado(int idEmpleado) {
 		this.idEmpleado = idEmpleado;
 	}
 
 	/**
+	 * @return El toString genera el contenido del objeto en una linea de texto
+	 *         creada a mano.
 	 * 
 	 */
 	public String toString() {
