5c4d0964abcd4c00feb4fc483c570994d472e758
1AW3 User 22 <i_salazargir@fpzornotza.com>
2025-05-09 11:07:00 +0200

    Ventanas finish

---
+++ Programacion/RepeGames/src/Vista/Confirmacion.java
@@ -0,0 +1,44 @@
+package Vista;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Confirmacion {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Confirmacion window = new Confirmacion();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Confirmacion() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+	}
+
+}
