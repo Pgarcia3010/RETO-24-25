1a84c0deb4f906dcd1c83cd38ed36135c15cc067
1AW3 User 22 <i_salazargir@fpzornotza.com>
2025-05-09 10:12:04 +0200

    Nuevas ventanas full

---
+++ Programacion/RepeGames/src/Vista/MenuEmpleados.java
@@ -0,0 +1,92 @@
+package Vista;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JTable;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import java.awt.FlowLayout;
+import javax.swing.SwingConstants;
+import javax.swing.JPanel;
+import javax.swing.JToggleButton;
+import java.awt.Font;
+import java.awt.Color;
+
+public class MenuEmpleados {
+
+	private JFrame frame;
+	public JButton BtncopiasSegu;
+	public JButton Btnpedidos;
+	public JButton Btnproductos;
+	
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenuEmpleados window = new MenuEmpleados();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MenuEmpleados() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 373, 267);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JButton Btnproductos = new JButton("Producto");
+		Btnproductos.setForeground(new Color(128, 0, 255));
+		Btnproductos.setBackground(new Color(0, 0, 0));
+		Btnproductos.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 16));
+		frame.getContentPane().add(Btnproductos);
+		
+		Btnpedidos = new JButton("Pedidos");
+		Btnpedidos.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 16));
+		Btnpedidos.setBackground(new Color(0, 0, 0));
+		Btnpedidos.setForeground(new Color(128, 0, 255));
+		Btnpedidos.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 16));
+		frame.getContentPane().add(Btnpedidos);
+		
+		BtncopiasSegu = new JButton("Copias de seguridad");
+		BtncopiasSegu.setForeground(new Color(128, 0, 255));
+		BtncopiasSegu.setBackground(new Color(0, 0, 0));
+		BtncopiasSegu.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 14));
+		frame.getContentPane().add(BtncopiasSegu);
+		
+		JButton Btnvolver = new JButton("Volver");
+		Btnvolver.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+			}
+		});
+		Btnvolver.setForeground(new Color(128, 0, 255));
+		Btnvolver.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 16));
+		Btnvolver.setBackground(new Color(0, 0, 0));
+		frame.getContentPane().add(Btnvolver);
+	}
+
+}
---
+++ Programacion/RepeGames/src/Vista/TablaAdministrarProductos.java
@@ -0,0 +1,117 @@
+package Vista;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+
+public class TablaAdministrarProductos {
+
+	private JFrame frame;
+	private JTable table;
+	private JTextField ResultadoId;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TablaAdministrarProductos window = new TablaAdministrarProductos();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TablaAdministrarProductos() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 852, 526);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel Titulo = DefaultComponentFactory.getInstance().createLabel("Administrar Productos");
+		Titulo.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 24));
+		Titulo.setBounds(10, 10, 278, 29);
+		frame.getContentPane().add(Titulo);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(42, 63, 745, 229);
+		frame.getContentPane().add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		
+		JButton btnFiltro = new JButton("Filtro ID");
+		btnFiltro.setBackground(new Color(240, 240, 240));
+		btnFiltro.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 12));
+		btnFiltro.setBounds(266, 302, 101, 21);
+		frame.getContentPane().add(btnFiltro);
+		
+		JButton btnFiltro2 = new JButton("Filtro Stock");
+		btnFiltro2.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 12));
+		btnFiltro2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnFiltro2.setBounds(401, 302, 111, 21);
+		frame.getContentPane().add(btnFiltro2);
+		
+		JButton btnVolver = new JButton("Volver");
+		btnVolver.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+			}
+		});
+		btnVolver.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 14));
+		btnVolver.setBounds(22, 458, 85, 21);
+		frame.getContentPane().add(btnVolver);
+		
+		ResultadoId = new JTextField();
+		ResultadoId.setBounds(148, 380, 158, 19);
+		frame.getContentPane().add(ResultadoId);
+		ResultadoId.setColumns(10);
+		
+		JLabel titulo = DefaultComponentFactory.getInstance().createLabel("Id Producto:");
+		titulo.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 14));
+		titulo.setBounds(148, 367, 101, 13);
+		frame.getContentPane().add(titulo);
+		
+		JButton btnseleccionar = new JButton("Selecionado");
+		btnseleccionar.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 12));
+		btnseleccionar.setBounds(440, 361, 111, 83);
+		frame.getContentPane().add(btnseleccionar);
+		
+		JButton btnEliminar = new JButton("Eliminar");
+		btnEliminar.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 12));
+		btnEliminar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnEliminar.setBounds(590, 361, 101, 83);
+		frame.getContentPane().add(btnEliminar);
+	}
+}
