1f54d502afb58cc3bf500ec70dac24068563458f
Paul <p_garciaveg@fpzornotza.com>
2025-05-14 09:19:36 +0200

    Eliminaron comentarios

--- Programacion/RepeGames/src/appPrincipal.java
+++ Programacion/RepeGames/src/appPrincipal.java
@@ -1,31 +1,3 @@
-//import Controlador.Controlador;
-//import Modelo.Modelo;
-//import Vista.VentanaLogin;
-//
-//public class appPrincipal {
-//
-//	public static void main(String[] args) {
-//		//de aqui a la ventana y de alli al controlador
-//	
-//	Controlador controlador = new Controlador();
-//	
-//	Modelo modelo = new Modelo();
-//	VentanaLogin ventanaLogin = new VentanaLogin();
-//	ventanaLogin.setControlador(controlador); 
-//	
-//	controlador.setModelo(modelo);
-//	controlador.setLogin(ventanaLogin);
-//	controlador.recibirClientes();
-//	controlador.recibirEmpleados();
-//	
-////	ventanaLogin.setVisible(true);
-//	
-//	ventanaLogin.mostrarVentana();
-//	
-//	
-//	}
-//
-//}
 import java.sql.SQLException;
 
 import Controlador.Controlador;
