31702fbf54ab4fc26b4f73f73196dc9fa6011631
Paul <p_garciaveg@fpzornotza.com>
2025-05-14 10:34:29 +0200

    Prueba

--- Programacion/RepeGames/src/Vista/TablaAdministrarProductos.java
+++ Programacion/RepeGames/src/Vista/TablaAdministrarProductos.java
@@ -1,19 +1,19 @@
 package Vista;
 
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import com.jgoodies.forms.factories.DefaultComponentFactory;
-import java.awt.BorderLayout;
-import java.awt.Font;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.JButton;
-import java.awt.Color;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
+
+import com.jgoodies.forms.factories.DefaultComponentFactory;
 
 public class TablaAdministrarProductos {
 
